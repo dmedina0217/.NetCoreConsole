ed1e1f94cb17041334a1460eb94dfb99a2468116
dmedina0217 <dmedina0217@gmail.com>
2016-09-21 17:04:32 -0500

    Simple SQL Lite DB using .NetCore

---
+++ Database/Migrations/BloggingContextModelSnapshot.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NetCoreDatabaseProject;
+
+namespace Database.Migrations
+{
+    [DbContext(typeof(BloggingContext))]
+    partial class BloggingContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("NetCoreDatabaseProject.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("BlogId");
+
+                    b.ToTable("Blogs");
+                });
+
+            modelBuilder.Entity("NetCoreDatabaseProject.Post", b =>
+                {
+                    b.Property<int>("PostId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BlogId");
+
+                    b.Property<string>("Content");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("PostId");
+
+                    b.HasIndex("BlogId");
+
+                    b.ToTable("Posts");
+                });
+
+            modelBuilder.Entity("NetCoreDatabaseProject.Post", b =>
+                {
+                    b.HasOne("NetCoreDatabaseProject.Blog", "Blog")
+                        .WithMany("Posts")
+                        .HasForeignKey("BlogId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
--- Database/Program.cs
+++ Database/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace ConsoleApplication
+namespace NetCoreDatabaseProject
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            using(var db=new BloggingContext()){
+                Console.WriteLine("Enter your Blog Name");
+                var response=Console.ReadLine();
+
+                db.Blogs.Add(new Blog{Name=response});
+                db.SaveChanges();
+
+                Console.WriteLine("Blogs in database: ");
+                foreach(var blog in db.Blogs){
+                    Console.WriteLine(blog.Name);
+                }
+            }
         }
     }
 }
