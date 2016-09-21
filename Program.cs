04d9ed4186b9a640b409f323bef091b6ea9d4528
dmedina0217 <dmedina0217@gmail.com>
2016-09-21 10:55:43 -0500

    Reading and Writing To a File

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApplication
 {
@@ -7,6 +8,35 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("This is from .NET Core !");
+            Console.WriteLine();
+            Console.WriteLine("Writing and Reading From A File");
+            Console.WriteLine();
+            //Create a new file
+
+            string information = "hello I am in a test file. This was written from Visual Studio Code";
+            File.WriteAllText("/Users/Danny/Source/Repos/.NetCore Console Projects/test1.txt",information);
+
+            //Read Contents of File
+            string fileContents=File.ReadAllText("/Users/Danny/Source/Repos/.NetCore Console Projects/test1.txt");
+            Console.WriteLine(fileContents);
+            Console.WriteLine("Everything Worked!");
+
+            //Writing To Array
+            string[] arrayInfo=new string[2];
+            arrayInfo[0]="this is line 1";
+            arrayInfo[1]="this is line 2";
+            File.WriteAllLines("/Users/Danny/Source/Repos/.NetCore Console Projects/arrayTest.txt",arrayInfo);
+
+            //Reading From Array File
+            string[] fileArray = File.ReadAllLines("/Users/Danny/Source/Repos/.NetCore Console Projects/arrayTest.txt");
+            //Loop through each line
+            foreach(string n in fileArray){
+                Console.WriteLine("File Array Contents: "+n);
+
+            }
+            Console.WriteLine("Exiting...");
+
+             
 	    
         }
     }
