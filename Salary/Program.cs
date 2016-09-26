fa3f16985024f8efabe9406bb8c9924af2b16a9f
dmedina0217 <dmedina0217@gmail.com>
2016-09-26 10:44:07 -0500

    Salary with Deductions

--- Salary/Program.cs
+++ Salary/Program.cs
@@ -22,20 +22,13 @@ namespace ConsoleApplication
             float WeeklyHours= Convert.ToSingle(WeeklyHoursReading);
             Console.WriteLine("You Entered: " + WeeklyHours);
 
-            //Tax Rates
-            Console.WriteLine("Taxes for Illinois: Effective January 1, 2015, the rate for withholding Illinois Income Tax is 3.75 percent.");
-            Console.WriteLine("Estimated Tax Percentage: ");
-            var taxReading= Console.ReadLine();
-            float taxes= Convert.ToSingle(taxReading);
-            Console.WriteLine(taxes);
-
             
             float weeklyPay = (hourlyPay * WeeklyHours);
             //Console.WriteLine(weeklyPay);
             
             //tax calculation
-            float taxCalculation= (weeklyPay * taxes / 100);
-            Console.WriteLine("Total Tax Taken Away: " + taxCalculation.ToString("C", CultureInfo.CurrentCulture));
+            //float taxCalculation= (weeklyPay * taxes / 100);
+            //Console.WriteLine("Total Tax Taken Away: " + taxCalculation.ToString("C", CultureInfo.CurrentCulture));
             Console.WriteLine("     Before Taxes: ");
             Console.WriteLine();
             Console.WriteLine("Weekly Pay Before Taxes: " + weeklyPay.ToString("C", CultureInfo.CurrentCulture));
@@ -44,12 +37,42 @@ namespace ConsoleApplication
             Console.WriteLine("     After Taxes: ");
             Console.WriteLine();
 
-            float result = weeklyPay - (weeklyPay * taxes / 100);
-            Console.WriteLine("Weekly Pay After Taxes: " + result.ToString("C", CultureInfo.CurrentCulture));
+            //float result = weeklyPay - (weeklyPay * taxes / 100);
+            //Console.WriteLine("Weekly Pay After Taxes: " + result.ToString("C", CultureInfo.CurrentCulture));
             
                         
             // float weeklyPayTaxes = 
             //Console.WriteLine("Weekly Pay After Taxes: " weeklyPay);
+
+            //mandatory taxes
+            Console.WriteLine();
+            //Console.WriteLine("*"*30);
+            Console.WriteLine("Mandatory Taxes: " );
+            double fica= 5.77218270310745 * weeklyPay /100;
+            Console.WriteLine("FICA: " + fica.ToString("C", CultureInfo.CurrentCulture));
+            double ficaMedicare=1.350617746162486 * weeklyPay / 100;
+            Console.WriteLine("FICA Medica: " + ficaMedicare.ToString("C", CultureInfo.CurrentCulture));
+            double ficaTax= 8.593223511793336 * weeklyPay / 100;
+            Console.WriteLine("FICA Tax: " + ficaTax.ToString("C", CultureInfo.CurrentCulture));
+            double stateTax=3.269374766005241 * weeklyPay / 100;
+            Console.WriteLine("State Tax: " + stateTax.ToString("C", CultureInfo.CurrentCulture));
+            Console.WriteLine();
+
+            double totalDeductions = fica + ficaMedicare + ficaTax + stateTax;
+            
+            Console.WriteLine("Total Deducations: " + totalDeductions.ToString("C", CultureInfo.CurrentCulture));
+            //Console.WriteLine("*"*);
+            Console.WriteLine();
+
+            double weeklyPayAfterDeductions=weeklyPay - totalDeductions;
+            double monthlyPayAfterDeductions= weeklyPayAfterDeductions * 4;
+            double yearlyPayAfterDeductions= weeklyPayAfterDeductions * 52;
+            Console.WriteLine("Weekly Pay After Deductions: " + weeklyPayAfterDeductions.ToString("C", CultureInfo.CurrentCulture));
+            Console.WriteLine("Monthly Pay After Deductions: " + monthlyPayAfterDeductions.ToString("C", CultureInfo.CurrentCulture));
+            Console.WriteLine("Yearly Pay After Deductions: " + yearlyPayAfterDeductions.ToString("C", CultureInfo.CurrentCulture));
+
+
+
 
 
 
